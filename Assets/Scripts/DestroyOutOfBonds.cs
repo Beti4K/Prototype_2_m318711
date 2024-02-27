3275f3a2a865390a60ce35f81d3744595d39417e
Beti4K <beata.kolodziejczyk2004@gmail.com>
2024-02-27 14:47:41 +0000

    Destroy Animals
    Destroying Animals when out of bounds

--- Assets/Scripts/DestroyOutOfBonds.cs
+++ Assets/Scripts/DestroyOutOfBonds.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class DestroyOutOfBonds : MonoBehaviour
 {
     private float topBound = 30;
+    private float lowerBound = -10;
     void Update()
     {
         if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
         {
             Destroy(gameObject);
         }
