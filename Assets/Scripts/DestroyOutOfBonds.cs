b6507ebf4e9e4f289637d9e2e3f16459823243bd
Beti4K <beata.kolodziejczyk2004@gmail.com>
2024-02-27 15:44:24 +0000

    Game Over
    Adding "Game Over" when losing the game

--- Assets/Scripts/DestroyOutOfBonds.cs
+++ Assets/Scripts/DestroyOutOfBonds.cs
@@ -14,6 +14,7 @@ public class DestroyOutOfBonds : MonoBehaviour
         }
         else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
